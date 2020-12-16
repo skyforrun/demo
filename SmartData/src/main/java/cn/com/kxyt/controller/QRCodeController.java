6d5859d0873882ca4c01791987fb2b9b50a2e564
runforsky <18717121440>
2020-12-16 16:59:10 +0800

    我很烦

--- SmartData/src/main/java/cn/com/kxyt/controller/QRCodeController.java
+++ SmartData/src/main/java/cn/com/kxyt/controller/QRCodeController.java
@@ -42,14 +42,13 @@ public class QRCodeController {
             @ApiResponse(code=400,message="请求参数没填好"),
             @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
     })
-    @ExtApiIdempotent(Constant.EXTAPIFROM)
-    public StandAddress productcode(@PathVariable String id) {
+    public cn.com.kxyt.core.Result productcode(@PathVariable String id) {
         logger.info("开始生成二维码,id为:"+id);
         QRCodeUtil.zxingCodeCreate("http://192.168.1.12/code/test/"+id, "./public/qrcode/"+id,500,null);
         logger.info("生成二维码成功");
         StandAddress address = new StandAddress();
         address.setImageSrc("http://192.168.1.12/"+id+".jpg");
-        return address;
+        return cn.com.kxyt.core.Result.success(address);
     }
 
 
@@ -65,14 +64,14 @@ public class QRCodeController {
             @ApiResponse(code=400,message="请求参数没填好"),
             @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
     })
-    public StandAddress analysiscode(@PathVariable String id) {
+    public cn.com.kxyt.core.Result analysiscode(@PathVariable String id) {
         logger.info("开始解析二维码，ID为:"+id);
         Result result = QRCodeUtil.zxingCodeAnalyze("./public/qrcode/"+id+".jpg");
         //取出地址栏的唯一参数ID
         String uid = result.toString().substring(result.toString().lastIndexOf("/")+1);
         StandAddress standAddress = addressMapper.selectStandAddressById(Integer.valueOf(uid));
         logger.info("ID为"+id+"解析的结果为："+standAddress);
-        return standAddress;
+        return cn.com.kxyt.core.Result.success(standAddress);
     }
 
 }

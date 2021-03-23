2e9466f342b3499ec9418fa325b42121abda2c81
zengjun <zengjun@sungotech.com>
2021-03-23 14:20:04 +0800

    1.修复flowable写流程图乱码问题

--- SmartData/src/main/java/com/hgnu/study/controller/flowable/ExpenseController.java
+++ SmartData/src/main/java/com/hgnu/study/controller/flowable/ExpenseController.java
@@ -1,5 +1,11 @@
 package com.hgnu.study.controller.flowable;
 
+import com.hgnu.study.core.Result;
+import com.hgnu.study.exception.TipException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.flowable.bpmn.model.BpmnModel;
 import org.flowable.engine.*;
 import org.flowable.engine.runtime.Execution;
@@ -8,10 +14,13 @@ import org.flowable.image.ProcessDiagramGenerator;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -26,6 +35,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "expense")
+@Api(value = "flowable自定义报销流程示例",tags = "flowable工作流demo示例")
 public class ExpenseController {
 
     @Autowired
@@ -44,28 +54,37 @@ public class ExpenseController {
      * @param money     报销金额
      * @param descption 描述
      */
-    @RequestMapping(value = "add")
+    @PostMapping(value = "add")
     @ResponseBody
-    public String addExpense(String userId, Integer money, String descption) {
+    @ApiOperation(value="开始流程")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="userId",value="用户id"),
+            @ApiImplicitParam(name="money",value="报销金额"),
+            @ApiImplicitParam(name="descption",value="描述")
+    })
+    public Result addExpense(String userId, Integer money, String descption) {
         //启动流程
         HashMap<String, Object> map = new HashMap<>();
         map.put("taskUser", userId);
         map.put("money", money);
+        map.put("descption", descption);
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Expense", map);
-        return "提交成功.流程Id为：" + processInstance.getId();
+        return Result.success("流水号id为:"+processInstance.getId());
     }
 
     /**
      * 获取审批管理列表
      */
-    @RequestMapping(value = "/list")
+    @PostMapping(value = "/list")
     @ResponseBody
-    public Object list(String userId) {
+    @ApiOperation(value="查询流程列表,待办列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="userId",value="用户id")
+    })
+    public Result list(String userId) {
         List<Task> tasks = taskService.createTaskQuery().taskAssignee(userId).orderByTaskCreateTime().desc().list();
-        for (Task task : tasks) {
-            System.out.println(task.toString());
-        }
-        return tasks.toArray().toString();
+        tasks.stream().forEach(e -> System.out.println(e.toString()));
+        return Result.success(tasks.toString());
     }
 
     /**
@@ -73,30 +92,38 @@ public class ExpenseController {
      *
      * @param taskId 任务ID
      */
-    @RequestMapping(value = "apply")
+    @PostMapping(value = "apply")
     @ResponseBody
-    public String apply(String taskId) {
+    @ApiOperation(value="批准同意")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="taskId",value="任务id")
+    })
+    public Result apply(String taskId) {
         Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
         if (task == null) {
-            throw new RuntimeException("流程不存在");
+            throw new TipException("流程不存在");
         }
         //通过审核
         HashMap<String, Object> map = new HashMap<>();
         map.put("outcome", "通过");
         taskService.complete(taskId, map);
-        return "processed ok!";
+        return Result.success();
     }
 
     /**
      * 拒绝
      */
     @ResponseBody
-    @RequestMapping(value = "reject")
-    public String reject(String taskId) {
+    @PostMapping(value = "reject")
+    @ApiOperation(value="拒绝不同意")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="taskId",value="任务id")
+    })
+    public Result reject(String taskId) {
         HashMap<String, Object> map = new HashMap<>();
         map.put("outcome", "驳回");
         taskService.complete(taskId, map);
-        return "reject";
+        return Result.success("驳回,reject!!!");
     }
 
     /**
@@ -104,7 +131,11 @@ public class ExpenseController {
      *
      * @param processId 任务ID
      */
-    @RequestMapping(value = "processDiagram")
+    @GetMapping(value = "processDiagram")
+    @ApiOperation(value="生成流水图")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="processId",value="流水号id")
+    })
     public void genProcessDiagram(HttpServletResponse httpServletResponse, String processId) throws Exception {
         ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(processId).singleResult();
 
@@ -133,14 +164,21 @@ public class ExpenseController {
         ProcessEngineConfiguration engconf = processEngine.getProcessEngineConfiguration();
         ProcessDiagramGenerator diagramGenerator = engconf.getProcessDiagramGenerator();
         InputStream in = diagramGenerator.generateDiagram(bpmnModel,"png",activityIds,flows,engconf.getActivityFontName(),engconf.getLabelFontName(),engconf.getAnnotationFontName(),engconf.getClassLoader(),1.0,true);
+
+        //设置响应的类型格式为图片
+        httpServletResponse.setContentType("image/png");
+        //禁止图像缓存
+        httpServletResponse.setHeader("Pragma","no-cache");
+        httpServletResponse.setHeader("Cache-Control","no-cache");
+        httpServletResponse.setDateHeader("Expires",0);
+
+        //写出
         OutputStream out = null;
-        byte[] buf = new byte[1024];
-        int legth;
         try {
             out = httpServletResponse.getOutputStream();
-            while ((legth = in.read(buf)) != -1) {
-                out.write(buf, 0, legth);
-            }
+            BufferedImage image = null;
+            image = ImageIO.read(in);
+            ImageIO.write(image,"png",out);
         } finally {
             if (in != null) {
                 in.close();

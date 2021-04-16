cf5602101a3650c15864981bcd1aa8f3c1607c93
zengjun <zengjun@sungotech.com>
2021-04-16 11:46:06 +0800

    分支测试

---
+++ SmartData/src/test/java/com/hgnu/study/psvm/T.java
@@ -0,0 +1,12 @@
+package com.hgnu.study.psvm;
+
+/**
+ * @Author zj
+ * @Description
+ * @Date 2021/4/16 11:45
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println("ds");
+    }
+}
